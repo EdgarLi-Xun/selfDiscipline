8ade47c428437ef0f34a6594d836d1779e177375
lixun <lixun21521>
2020-08-28 11:07:58 +0800

    初始提交

---
+++ SD-web/src/main/java/com/edgarli/SD/SelfDisciplineApplication.java
@@ -0,0 +1,13 @@
+package com.edgarli.SD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfDisciplineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfDisciplineApplication.class, args);
+    }
+
+}
---
+++ SD-web/src/test/java/com/edgarli/xun/SD/SelfDisciplineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edgarli.xun.SD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfDisciplineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
